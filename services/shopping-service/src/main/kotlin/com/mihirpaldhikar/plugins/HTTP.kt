b23cded1490c4e7aee4d191233deba94964715ef
Mihir Paldhikar <imihirpaldhikar@gmail.com>
2024-06-19 10:49:03 +0530

    chore (shopping-service): Add CORS policies for accessing the routes.

--- services/shopping-service/src/main/kotlin/com/mihirpaldhikar/plugins/HTTP.kt
+++ services/shopping-service/src/main/kotlin/com/mihirpaldhikar/plugins/HTTP.kt
@@ -1,13 +1,18 @@
 package com.mihirpaldhikar.plugins
 
+import com.mihirpaldhikar.Environment
 import io.ktor.http.*
 import io.ktor.http.content.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.cachingheaders.*
 import io.ktor.server.plugins.compression.*
 import io.ktor.server.plugins.cors.routing.*
+import org.koin.java.KoinJavaComponent
 
 fun Application.configureHTTP() {
+
+    val environment by KoinJavaComponent.inject<Environment>(Environment::class.java)
+
     install(CachingHeaders) {
         options { _, outgoingContent ->
             when (outgoingContent.contentType?.withoutParameters()) {
@@ -26,11 +31,26 @@ fun Application.configureHTTP() {
         }
     }
     install(CORS) {
+        allowCredentials = true
+
         allowMethod(HttpMethod.Options)
-        allowMethod(HttpMethod.Put)
-        allowMethod(HttpMethod.Delete)
+        allowMethod(HttpMethod.Get)
+        allowMethod(HttpMethod.Post)
         allowMethod(HttpMethod.Patch)
+        allowMethod(HttpMethod.Delete)
+
+        allowHeader(HttpHeaders.Origin)
+        allowHeader(HttpHeaders.Accept)
+        allowHeader(HttpHeaders.ContentType)
         allowHeader(HttpHeaders.Authorization)
-        anyHost()
+        allowHeader(HttpHeaders.AuthenticationInfo)
+        allowHeader(HttpHeaders.AccessControlAllowOrigin)
+        allowHeader(HttpHeaders.AccessControlAllowHeaders)
+        allowHeader(HttpHeaders.AccessControlAllowCredentials)
+
+        if (environment.developmentMode) {
+            allowHost("localhost:3000", listOf("http", "https"))
+        }
+
     }
 }
