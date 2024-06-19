b0e19ac769a3dc7d70b171e9799c987d550c1bf3
Mihir Paldhikar <imihirpaldhikar@gmail.com>
2024-06-19 11:45:34 +0530

    chore (shopping-service): Add authentication credentials and base url config in Environment.kt

--- services/shopping-service/src/main/kotlin/com/mihirpaldhikar/Environment.kt
+++ services/shopping-service/src/main/kotlin/com/mihirpaldhikar/Environment.kt
@@ -1,5 +1,16 @@
 package com.mihirpaldhikar
 
+import com.mihirpaldhikar.entities.Credentials
+
 class Environment {
     val developmentMode: Boolean = System.getenv("DEVELOPMENT_MODE")?.toBoolean() ?: false
+    val baseURL: String = System.getenv("BASE_URL")
+    val credentials = Credentials(
+        companyName = System.getenv("COMPANY_NAME").toString(),
+        clientID = System.getenv("CLIENT_ID").toString(),
+        clientSecret = System.getenv("CLIENT_SECRET").toString(),
+        ownerName = System.getenv("OWNER_NAME").toString(),
+        ownerEmail = System.getenv("OWNER_EMAIL").toString(),
+        rollNo = System.getenv("ROLL_NO").toString()
+    )
 }
